dcd202a9a92c3b2c87275030b3f56766ebadf0b6
Suny <inbox@xieziming.com>
2016-08-07 03:37:55 +0800

    daily update

--- stap-db/src/main/resources/triggers/test_data.sql
+++ stap-db/src/main/resources/triggers/test_data.sql
@@ -5,7 +5,7 @@ DROP TRIGGER IF EXISTS `test_data_insert_trigger`;
 DELIMITER $$
 CREATE TRIGGER `test_data_insert_trigger` AFTER INSERT ON `test_data`
 FOR EACH ROW
-  INSERT INTO test_case_revision SET Test_Case_Id = NEW.Test_Case_Id, Type = 'Insert', Content = CONCAT('Insert data : ', NEW.Test_Data_Definition_Id);
+  INSERT INTO test_case_revision SET Test_Case_Id = NEW.Test_Case_Id, Type = 'Insert', Content = CONCAT('Insert data(', NEW.Id, ', ', (SELECT Field FROM test_data_definition WHERE Id = NEW.Test_Data_Definition_Id), ')');
 $$
 DELIMITER ;
 
@@ -15,7 +15,7 @@ CREATE TRIGGER `test_data_update_trigger` AFTER UPDATE ON `test_data`
 FOR EACH ROW
   BEGIN
     IF OLD.Test_Data_Definition_Id != NEW.Test_Data_Definition_Id THEN
-      INSERT INTO test_case_revision SET Test_Case_Id = NEW.Test_Case_Id, Type = 'Update', Content = CONCAT('Update data to: ', NEW.Test_Data_Definition_Id , ', from: ', OLD.Test_Data_Definition_Id);
+      INSERT INTO test_case_revision SET Test_Case_Id = NEW.Test_Case_Id, Type = 'Update', Content = CONCAT('Update data(', OLD.Test_Data_Definition_Id, ', ', (SELECT Field FROM test_data_definition WHERE Id = OLD.Test_Data_Definition_Id), ') to data(', NEW.Test_Data_Definition_Id, ', ', (SELECT Field FROM test_data_definition WHERE Id = NEW.Test_Data_Definition_Id), ')' );
     END IF ;
   END
 $$
@@ -25,6 +25,6 @@ DROP TRIGGER IF EXISTS `test_data_delete_trigger`;
 DELIMITER $$
 CREATE TRIGGER `test_data_delete_trigger` AFTER DELETE ON `test_data`
 FOR EACH ROW
-    INSERT INTO test_case_revision SET Test_Case_Id = OLD.Test_Case_Id, Type = 'Delete', Content = CONCAT('Delete data : ', OLD.Test_Data_Definition_Id)
+    INSERT INTO test_case_revision SET Test_Case_Id = OLD.Test_Case_Id, Type = 'Delete', Content = CONCAT('Delete data(', OLD.Id, ', ', (SELECT Field FROM test_data_definition WHERE Id = OLD.Test_Data_Definition_Id), ')')
 $$
 DELIMITER ;
